60b3ee77e3142d12ea70c0b6ad3c679c938ea0bc
UlisesVeraC <veracuevasu@gmail.com>
2025-04-18 14:14:57 -0600

    updated

--- src/app/gifs/components/side-menu/navbar-options/navbar-options.component.ts
+++ src/app/gifs/components/side-menu/navbar-options/navbar-options.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+interface MenuOptions {
+  icon: string;
+  label: string;
+  route: string;
+  sublabel: string;
+}
+
 
 @Component({
   selector: 'app-navbar-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar-options.component.html',
 })
 export class NavbarOptionsComponent {
+
+  menuOptions: MenuOptions[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      sublabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      sublabel: 'Buscar gifs',
+      route: '/dashboard/search',
+    },
+  ]
 
 }
---
+++ src/app/gifs/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [SideMenuComponent],
+  templateUrl: './dashboard.component.html'
+})
+export default class DashboardComponent {
+
+}
