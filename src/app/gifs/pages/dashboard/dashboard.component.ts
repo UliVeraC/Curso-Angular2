6586d4f479ba7ba3abf75483b0fb000cd1983aa8
UlisesVeraC <veracuevasu@gmail.com>
2025-04-18 19:51:09 -0600

    Images done

--- src/app/gifs/pages/dashboard/dashboard.component.ts
+++ src/app/gifs/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [SideMenuComponent],
+  imports: [SideMenuComponent, RouterOutlet],
   templateUrl: './dashboard.component.html'
 })
 export default class DashboardComponent {
